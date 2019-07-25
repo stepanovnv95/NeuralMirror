47fe2a6920973ba4c9bdf32d71774ad39cb111b6
Nikita Stepanov <stepanovnv95@gmail.com>
2019-07-25 15:36:01 +0400

    Сделал нормальный фильтр close #2

--- workers/filter_worker.py
+++ workers/filter_worker.py
@@ -1,36 +1,38 @@
 from PySide2.QtCore import QObject, Signal, Slot
-from time import time
 
 
 class FilterWorker(QObject):
 
-    predict_values_updated_signal = Signal(dict)
+    predict_values_updated_signal: Signal = Signal(dict)
 
-    def __init__(self, labels:list):
+    def __init__(self, labels: list):
         super().__init__()
-        self.labels = dict((key, 0.0) for key in labels)
-        self.default_key = 'no_one'
-        self.last_time = time()
-        self.kt = 2.0
+        self.values = {l: 0.0 for l in labels}
+        self.k = 0.2
+        self.last_label = labels[0]
+        self.hysteresis = (0.4, 0.7)
 
     @Slot(dict)
-    def update_predict_values(self, predict_dict:dict):
-        current_time = time()
-        dt = current_time - self.last_time
-        dt = min(dt, self.kt)
-        dv = dt / self.kt
-        self.last_time = current_time
-        max_label = self.default_key if self.default_key in self.labels.keys() else list(self.labels.keys())[0]
-        max_value = 0.0
+    def update_predict_values(self, predict_dict: dict):
         for key, value in predict_dict.items():
-            if value > 0.7:
-                self.labels[key] += dv
-                self.labels[key] = min(self.labels[key], 1.0)
-            else:
-                self.labels[key] -= dv
-                self.labels[key] = max(self.labels[key], 0.0)
-            if self.labels[key] > max_value:
-                max_label, max_value = key, self.labels[key]
-        result = dict((key, 0) for key, _ in self.labels.items())
-        result[max_label] = 1
+            self.values[key] = (1 - self.k) * self.values[key] + self.k * value
+            self.values[key] = round(self.values[key], 4)
+
+        last_label_value = self.values[self.last_label]
+        if last_label_value < self.hysteresis[0]:
+            max_label, max_value = self._max_in_dict(self.values)
+            if max_value > self.hysteresis[1]:
+                self.last_label = max_label
+
+        result = {label: 1 if label == self.last_label else 0 for label, _ in self.values.items()}
         self.predict_values_updated_signal.emit(result)
+
+    @staticmethod
+    def _max_in_dict(d: dict):
+        items = list(d.items())
+        max_key, max_value = items[0]
+        for key, value in items[1:]:
+            if value > max_value:
+                max_value = value
+                max_key = key
+        return max_key, max_value
