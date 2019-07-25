5b5a0186e6aa5ea1f5f3369dce2e42600e70a17a
Nikita Stepanov <stepanovnv95@gmail.com>
2019-07-25 10:48:43 +0400

    Добавил watchdog таймер и приветствия при отсутствии человка

--- workers/video_starter_worker.py
+++ workers/video_starter_worker.py
@@ -13,15 +13,20 @@ class VideoStarterWorker(AbstractWorker):
     videos = {}
     blocked = False
     no_one_timer = QTimer()
-    no_one_timeout = (5 * 1000, 10 * 1000)
+    no_one_timeout = (7 * 1000, 15 * 1000)
+    watchdog_timer = QTimer()
+    watchdog_timeout = 30 * 1000
 
     def __init__(self, labels: list, no_one_label: str):
         super().__init__()
-        self.last_label = no_one_label
+        self.last_label = None
         self.labels = labels
         self.no_one_label = no_one_label
         self.scan_videos()
         self.no_one_timer.setSingleShot(True)
+        self.no_one_timer.timeout.connect(self.start_no_one_video)
+        self.watchdog_timer.setSingleShot(True)
+        self.watchdog_timer.timeout.connect(self._watchdog)
 
     def scan_videos(self):
         for l in self.labels:
@@ -40,16 +45,31 @@ class VideoStarterWorker(AbstractWorker):
                 label = key
                 break
         if label != self.last_label:
-            self.last_label = label
-            self.start_video(label)
+
+            if label == self.no_one_label:
+                self.no_one_timer.start(random.randint(*self.no_one_timeout))
+            else:
+                self.no_one_timer.stop()
+                self.start_video(label)
+        self.last_label = label
+
+    @Slot()
+    def start_no_one_video(self):
+        self.start_video(self.no_one_label)
+        self.no_one_timer.start(random.randint(*self.no_one_timeout))
 
     def start_video(self, label: str):
-        if self.blocked or label == self.no_one_label:
+        if self.blocked:
             return
         self.blocked = True
+        self.watchdog_timer.start(self.watchdog_timeout)
         videos = self.videos[label]
         v = videos[random.randint(0, len(videos) - 1)]
         self.set_video_signal.emit(v)
+
+    @Slot()
+    def _watchdog(self):
+        self.blocked = False
 
     @Slot()
     def unblock(self):
